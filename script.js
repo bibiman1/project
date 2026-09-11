c182b5f0c0675f842161798a8cc2355d4a94638c
bibiman1 <bibiman@yahoo.co.jp>
2026-09-11 11:12:46 +0900

    Add files via upload
    初期版を登録

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("checkButton");
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+  result.textContent = "JavaScriptも正常に動作しています。";
+});
